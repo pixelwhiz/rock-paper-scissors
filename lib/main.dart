78d53164f85933d6596429e35f40888de001f9c2
M. Daffa Teuku F.A <111522987+pixelwhiz@users.noreply.github.com>
2024-01-26 00:26:01 +0700

    seconds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,121 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Batu Kertas Gunting Game',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyGamePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyGamePage extends StatefulWidget {
+  const MyGamePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyGamePage> createState() => _MyGamePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyGamePageState extends State<MyGamePage> {
+  int _playerScore = 0;
+  int _computerScore = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Batu Kertas Gunting Game'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'Player Score: $_playerScore',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
             Text(
-              '$_counter',
+              'Computer Score: $_computerScore',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _playGame(Choice.rock),
+              child: const Text('Batu'),
+            ),
+            ElevatedButton(
+              onPressed: () => _playGame(Choice.paper),
+              child: const Text('Kertas'),
+            ),
+            ElevatedButton(
+              onPressed: () => _playGame(Choice.scissors),
+              child: const Text('Gunting'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  void _playGame(Choice playerChoice) {
+    Choice computerChoice = _getRandomChoice();
+    String result = _calculateWinner(playerChoice, computerChoice);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Computer chose ${_choiceToString(computerChoice)}. $result'),
+      ),
+    );
+
+    setState(() {
+      if (result == 'Player wins!') {
+        _playerScore++;
+      } else if (result == 'Computer wins!') {
+        _computerScore++;
+      }
+    });
+  }
+
+  Choice _getRandomChoice() {
+    Random random = Random();
+    List<Choice> choices = Choice.values;
+    return choices[random.nextInt(choices.length)];
+  }
+
+  String _calculateWinner(Choice playerChoice, Choice computerChoice) {
+    if (playerChoice == computerChoice) {
+      return 'It\'s a tie!';
+    } else if ((playerChoice == Choice.rock && computerChoice == Choice.scissors) ||
+        (playerChoice == Choice.paper && computerChoice == Choice.rock) ||
+        (playerChoice == Choice.scissors && computerChoice == Choice.paper)) {
+      return 'Player wins!';
+    } else {
+      return 'Computer wins!';
+    }
+  }
+
+  String _choiceToString(Choice choice) {
+    switch (choice) {
+      case Choice.rock:
+        return 'Batu';
+      case Choice.paper:
+        return 'Kertas';
+      case Choice.scissors:
+        return 'Gunting';
+      default:
+        return '';
+    }
+  }
+}
+
+enum Choice {
+  rock,
+  paper,
+  scissors,
 }
